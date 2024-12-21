af634538a7abecd27056c66d8001f79666f1cb28
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-12-22 00:12:42 +0600

    firebase login logout setup

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../src/assets/banner-test.jpg'
+
+const Banner = () => {
+    return (
+        <div>
+            <img src={banner} alt="" />
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
+import Banner from '../Components/Banner';
 
 const Home = () => {
     return (
 
-       <h1>hdf</h1>
-                
-    //             {
-    //     user && (
-    //         <div style={{ textAlign: "center", marginTop: "0px" }}>
-    //             <div className="avatar mt-1 online">
-    //                 <div className="sm:max-w-16 max-w-10 rounded-full">
-    //                     <img referrerPolicy='no-referrer' src={user?.photoURL} alt="User Profile"
-    //                         data-tooltip-id="image-tooltip"
-    //                         data-tooltip-content="azmirkhan"
-    //                         style={{ cursor: "pointer" }}
-    //                     />
-    //                 </div>
-    //             </div>
-    //             <Tooltip id="image-tooltip" place="top" style={{ backgroundColor: "#333" }} />
-    //         </div>
-
-    //     )
-    // }
+      <Banner></Banner>
 
      )
 };
